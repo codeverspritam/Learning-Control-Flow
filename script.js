9c752110b99915d63bac490e2b67970ab87e961d
Pritam <itspritamprasad@gmail.com>
2025-11-06 10:51:29 +0530

    Add getGrade function to evaluate scores, corresponding grades

--- script.js
+++ script.js
@@ -198,6 +198,31 @@
   console.log(result);
 }
 
+// if else ...
+{
+function getGrade(score){
+  if(score >= 90 && score<= 100){
+    return'A';
+  }
+  else if (score >= 80 && score <= 89 ){
+    return'B';
+  }
+  else if (score >= 70 && score <= 79){
+    return'C';
+  }
+  else if (score >= 60 && score <= 69){
+    return'D';
+  }
+  else if (score >= 33 && score <= 59){
+    return'E'
+  }
+  else if (score >= 0 && score <= 32){
+    return'Fail'
+  }
+  else {"Invalid marks....."}
+}
+getGrade(85);
+}
 
 
 // end of script.js
