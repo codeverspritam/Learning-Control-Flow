6b39c486bd2cf3a2c9c7f2279ea8c2c7ce4b4c6d
Pritam <itspritamprasad@gmail.com>
2025-11-07 12:02:04 +0530

    break and continue structure

--- script.js
+++ script.js
@@ -301,7 +301,23 @@ do{
 while(i<20)
 }
 
+// break and continue
+{
+  /*break */
+  for (i=1; i<201; i++){
+    console.log(i);
+    if (i === 32){
+      break;
+    }
+  } 
 
+  /*continue */
+  for (i=1; i<201; i++){
+    if(i===32)
+    continue;
+  }
+  console.log(i);
+}
 
 
 
