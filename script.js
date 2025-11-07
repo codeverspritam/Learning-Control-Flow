f2c976d0acd5e3faf10facd07b1de5f628ca853d
Pritam <itspritamprasad@gmail.com>
2025-11-07 13:14:07 +0530

    Q9. Count how many numbers between 1 to 100 are divisible by both 3 and 5

--- script.js
+++ script.js
@@ -389,3 +389,13 @@
     }
   }
 }
+
+// Q9. Count how many numbers between 1 to 100 are divisible by both 3 and 5
+
+{
+  for (let i = 1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(i);
+    }
+  }
+}
