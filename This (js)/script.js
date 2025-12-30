6afd0a592e76fc9586a986bc568ab0d27c40a2f7
Pritam Prasad <itspritamprasad@gmail.com>
2025-12-30 06:35:58 +0000

    Add explanation of 'this' keyword and demonstrate its usage in an object method

--- This (js)/script.js
+++ This (js)/script.js
@@ -1,2 +1,12 @@
 // this keyword aak special keyword hai, kyuki jaise ki baaki saare keyword ki value ya unka nature same rehta hai this ki value ya nature badal jaata hai baat se ki aap usey kaha use kar rahe ho
-// commit-note: (this definition)
+
+//yahape this ki value method ke andar hai to this us object ko point karega jiske andar yeh method hai
+{
+    let obj = {
+        nmae: "Jhon",
+        sayName: function () {
+            console.log(this);
+        }
+    }
+    obj.sayName();
+}
