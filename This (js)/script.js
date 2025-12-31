3f1eda738c64fe9b0a87371ac6c56c3e3d147e70
Pritam Prasad <itspritamprasad@gmail.com>
2025-12-31 04:36:17 +0000

    Fix typo in object property name and enhance comments for clarity

--- This (js)/script.js
+++ This (js)/script.js
@@ -1,12 +1,17 @@
 // this keyword aak special keyword hai, kyuki jaise ki baaki saare keyword ki value ya unka nature same rehta hai this ki value ya nature badal jaata hai baat se ki aap usey kaha use kar rahe ho
 
 //yahape this ki value method ke andar hai to this us object ko point karega jiske andar yeh method hai
+// aak yesa functuion jo method ke ander ho use hm object  ke method kehte hai
 {
     let obj = {
-        nmae: "Jhon",
+        name: "Jhon",
         sayName: function () {
             console.log(this);
         }
     }
     obj.sayName();
 }
+
+/*This ki value  global scope mai > window  */
+/*This ki value  function scope mai > window  */
+/*This ki value  object ke method ke ander > object */
